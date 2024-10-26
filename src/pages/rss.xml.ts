7796dd5f59df67b51e7b589d1109fbda7b237100
Manel Arrufat <manelarrufat@gmail.com>
2024-10-26 21:27:33 +0200

    Arreglant error

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,7 @@
 import rss from '@astrojs/rss';
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
+
 import MarkdownIt from 'markdown-it';
 import sanitizeHtml from 'sanitize-html';
 
@@ -17,7 +18,8 @@ export const GET: APIRoute = async ({ params, request, site }) => {
         // `<description>` field in output xml
         description: 'Tot el que vols saber i més',
 
-        xmlns: { media: 'http://search.yahoo.com/mrss/', },
+        xmlns: { media: 'http://search.yahoo.com/mrss/',
+          },
 
         // Pull in your project "site" from the endpoint context
         // https://docs.astro.build/en/reference/api-reference/#contextsite
@@ -31,22 +33,20 @@ export const GET: APIRoute = async ({ params, request, site }) => {
             link: `/posts/${slug}`,
 
             content: sanitizeHtml(parser.render(body), {
-                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
-              }),
+              allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+            }),
               
-              customData: `<media:content
-                  type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
-                  width="${data.image.width}"
-                  height="${data.image.height}"
-                  medium="image"
-                  url="${site + data.image.src}" />
-              `,
+            customData: `<media:content
+                type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+                width="${data.image.width}"
+                height="${data.image.height}"
+                medium="image"
+                url="${site + data.image.src}" />
+            `,
         })),
         // (optional) inject custom xml
         customData: `<language>ca-es</language>`,
       });
 }
 
-function sanitizeHtml(arg0: any, arg1: { allowedTags: any; }) {
-    throw new Error('Function not implemented.');
-}
+
